23299865a9080238d005980700836e6c215488af
jeff <dh25093@gmail.com>
2025-08-17 16:07:58 -0500

    segundo comentario

--- ejemplo.py
+++ ejemplo.py
@@ -15,3 +15,38 @@ if edad >= 18:
  print("Eres mayor de edad")
 else:
  print("Eres menor de edad")
+
+
+
+
+
+
+
+
+ from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.post("/ingles")
+def saludo():
+    return "hello world"
+
+@app.route("/japoness")
+def salud():
+    return "konichiwa world"
+
+
+@app.route("/frances")
+def salu():
+    return "bonour jour"
+
+
+@app.route("/portugues")
+def sal():
+    return "bonito dia"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=34)
